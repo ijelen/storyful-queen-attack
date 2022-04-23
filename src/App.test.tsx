b3e3a4e597e2328611099998e86f4d0fd0bc1b88
Igor Jelen <igorjelen@gmail.com>
2022-04-23 20:59:58 +0100

    Install react-test-renderer for Jest snapshots
    npm install -D react-test-renderer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 
 import App from "./App";
 
@@ -7,4 +8,10 @@ test("renders Can't attack", () => {
   render(<App />);
   const element = screen.getByText(/Can't attack/i);
   expect(element).toBeInTheDocument();
+});
+
+test("Render App", () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
