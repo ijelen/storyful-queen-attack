050d2f1a0c956e1531bf3a5746115994d5b2eeba
Igor Jelen <igorjelen@gmail.com>
2022-04-23 20:50:04 +0100

    Add an edge case to canAttact
    Return false if both queens are on the same field

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,6 +1,9 @@
 import { Position } from "../ChessBoardField";
 
 export const canAttack = (white: Position, black: Position): boolean => {
+  if (white[0] === black[0] && white[1] === black[1]) {
+    return false;
+  }
   if (
     white[0] === black[0] ||
     white[1] === black[1] ||
